d36928cdad552da381774ff3ff7c433740dca7d3
Totofish <eanean120@gmail.com>
2017-10-14 02:31:51 +0800

    chore(upgrade): upgrade all package
    react 16 + next 4

--- pages/_document.js
+++ pages/_document.js
@@ -22,7 +22,7 @@ export default class MyDocument extends Document {
 			<html>
 				<Head>
 					<base href="/" />
-					<meta charset="utf-8" />
+					<meta charSet="utf-8" />
 					<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,minimal-ui" />
 					<meta name="apple-mobile-web-app-capable" content="yes" />
 					<meta name="apple-mobile-web-app-status-bar-style" content="black" />

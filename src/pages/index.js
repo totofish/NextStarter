2a91d0c609bc1a3fab39d0d8c06408c364456d6e
Evan <evan.yen@104.com.tw>
2018-01-15 17:33:53 +0800

    feat(Async): api 改成並發方式

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,29 +14,40 @@ import Layout from '../components/layout';
 
 class Index extends React.Component {
   static async getInitialProps({
-    store, isServer, pathname, query // eslint-disable-line no-unused-vars
+    store, isServer, pathname, query, req, res // eslint-disable-line no-unused-vars
   }) {
-    // Async await
-    const res = await fetch('https://api.github.com/repos/zeit/next.js');
-    const json = await res.json();
+    const stargazers = fetch('https://api.github.com/repos/zeit/next.js')
+      .then(r => r.json())
+      .then(json => json.stargazers_count)
+      .catch(() => 'stargazers error');
 
     // Async promise
     const actionIP = getIP();
     store.dispatch(actionIP);
 
-    return Promise.all([actionIP])
-      .then(value => ({ stars: json.stargazers_count, ip: value[0].ip }))
-      .catch(() => ({ stars: json.stargazers_count }));
+    // 處理錯誤時預設回應
+    // const actionIPPromise = actionIP.Promise.catch(() => ({ ip: 'xx.xx.xx.xx' }));
+
+    // 並發以加快回應
+    const data = await Promise.all([stargazers, actionIP])
+      .then(value => ({ stars: value[0], ip: value[1].ip }))
+      .catch(() => ({ stars: '', ip: '' }));
+
+    return data;
+
+    // 也可以直接回傳 Promise
+    // return Promise.all([actionIP, stargazers])
+    //   .then(value => ({ stars: json.stargazers_count, ip: value[0].ip }))
+    //   .catch(() => ({ stars: json.stargazers_count }));
 
     /*
+    // 其他寫法
     return actionIP.Promise.then((value) => {
       return { stars: json.stargazers_count };
     }, (error) => {
       return { stars: json.stargazers_count };
     });
-    */
 
-    /*
     return new Promise((resolve, reject) => {
       store.dispatch(getIP({
         callback: (res) => {
@@ -50,7 +61,7 @@ class Index extends React.Component {
   }
 
   static propTypes = {
-    stars: PropTypes.number.isRequired,
+    stars: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
     url: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
     ip: PropTypes.string.isRequired
   }

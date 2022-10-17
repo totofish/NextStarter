b82a3b1b6aa3143e6fb4afeeb8589eb1c2570e9e
Totofish <eanean120@gmail.com>
2022-10-18 01:12:41 +0800

    docs: readme file

--- server/middlewares/session.ts
+++ server/middlewares/session.ts
@@ -3,6 +3,10 @@ import expressSession from 'express-session'
 export default expressSession({
   name: 'next-site.sid',
   secret: process.env.SESSION_SECRET_KEY!,
+  /* Defaults to a new MemoryStore instance.
+     MemoryStore is purposely not designed for a production environment.
+     It will leak memory under most conditions, does not scale past a single process,
+     and is meant for debugging and developing. */
   // store
   resave: false,
   rolling: true,
